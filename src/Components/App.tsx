ef11cbee9d1a7f689b11718ad94842521c3f718d
vsamsonov <vsamsonov@phoenixit.ru>
2022-06-05 13:24:58 +0300

    Сделал стартовую страницу

--- src/Components/App.tsx
+++ src/Components/App.tsx
@@ -1,9 +1,22 @@
+import { useState } from 'react';
 import Style from './App.module.css';
 import Header from './Header/Header';
 const App = () => {
+  const [labelValue, setLabelValue] = useState('');
+
+  const labelChange = (e: any) => {
+    setLabelValue(e.target.value);
+  } 
+
   return (
     <div className={Style.AppWrapper}>
       <Header />
+      <div className={Style.Start_page}>
+        <p className={Style.h1}>Добро пожаловать на стартовую страницу поисковика по серивису StackOverflow!</p>
+        <p className={Style.h2}>Для начала работы напишите что-нибудь в поиске и нажмите кнопку "Поиск"</p>
+        <input className={Style.search_field} type='text' placeholder='Введите для поиска' onChange={labelChange} value={labelValue}/>
+        <button className={Style.search_button} type='submit' onClick={() => console.log(labelValue)}>Поиск</button>
+      </div>
     </div>
   )
 }
--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -1,9 +1,12 @@
 import Style from './Header.module.css';
 
-const Header = () => (
-  <div className={Style.Wrapper}>
-    
-  </div>
-);
+const Header = () => { 
+
+  return (
+    <div className={Style.Wrapper}>
+      <div className={Style.logo}>StackOverflow - Finder</div>
+    </div>
+  )
+}
 
 export default Header;
